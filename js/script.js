55dc0a5b6da4d60fdb5083eca29fcbdb2eb114a8
dilan dhakal <dilandhakal889@gmail.com>
2026-01-01 21:12:49 +0545

    add code

--- js/script.js
+++ js/script.js
@@ -142,22 +142,134 @@
 // console.log(d.getTimezoneOffset());
 
 
-let person = "Dilan";
-console.log(person.toLowerCase());
-console.log(person.toUpperCase());
-console.log(person.startsWith("d"));
-console.log(person.includes("D"));
-console.log(person.endsWith("a"));
-console.log(person.slice(0,4))
-console.log(person.replace("Dilan","Dhakal"));
+// let person = "Dilan";
+// console.log(person.toLowerCase());
+// console.log(person.toUpperCase());
+// console.log(person.startsWith("d"));
+// console.log(person.includes("D"));
+// console.log(person.endsWith("a"));
+// console.log(person.slice(0,4))
+// console.log(person.replace("Dilan","Dhakal"));
 
-const fruits = ["Apple","Banana","Mango","orange"];
+// const fruits = ["Apple","Banana","Mango","orange"];
 
-console.log(fruits.splice([0],[3]))
+// console.log(fruits.splice([0],[3]))
 
 
-fruits.forEach(items =>
-    console.log(items)
-)
+// fruits.forEach(items =>
+//     console.log(items)
+// )
+// console.log(fruits);
+
+
+
+
+// write code to add apple and banana at the end of array 
+
+let fruits = ["mango"];
+
+fruits.push("Apple","banana");
 console.log(fruits);
 
+// Remove the last element from the array and store it in a variable 
+
+let numbers = [10,20,30,40];
+
+const pushNum = numbers.pop();
+console.log(pushNum)
+
+
+// Replace red with blue in the array
+
+let colors = ["red","green","yellow"];
+
+colors.splice(0,1,"blue");
+console.log(colors);
+
+
+
+// insert monday at index 1.
+
+let days = ['Sunday',"Tuesday","wednesday"];
+days.splice(1,0,"Monday");
+console.log(days)
+
+// remove 2 elements starting from index 1
+let items = ["pen","pencil","eraser","sharpener"];
+
+items.splice(1,2);
+console.log(items);
+
+// replace "java" with "javascript
+let text = "java";
+console.log(text.replace("java","Javascript"));
+
+let sentence = "We like java";
+console.log(sentence.replace("java","Javascript"));
+
+// Create a date object for todays date and store it in a variable 
+
+
+const d = new Date;
+const todayDate = {
+    sYears : d.getFullYear(),
+    sDate : d.getDate(),
+    sDay : d.getDay()
+}
+
+console.log(todayDate);
+
+// Remove the last two elements from the array.
+let arr = [1,2,3,4,5];
+
+arr.pop();
+arr.pop();
+console.log(arr);
+
+// add dog and cat at the end of array
+let animals = ["cows"];
+animals.push("dog","cat");
+console.log(animals);
+
+// remove the first element from the array and store it in variable
+let nums = [5,6,7,8,9,9];
+let removeNum = nums.shift();
+console.log(removeNum);
+
+// add red at the start of the array.
+let Bows = ["blue","green"]
+Bows.unshift("red");
+console.log(Bows);
+
+// array - splice(insert/remove/replace)
+// insert B at index 1.
+ let letters = ["A","C","D"];
+letters.splice(1,0,"B");
+console.log(letters);
+
+// remove 1 element starting from index 2 
+let item = ["pen","book","eraser","scale"];
+item.splice(2,1);
+console.log(item);
+
+// replace january with jan
+
+let months = ["january","february","March"];
+months.splice(0,1,"Jan");
+console.log(months);
+
+// replace bad with good 
+let msg = "this is a bad idea";
+console.log(msg.replace("bad","good"));
+
+// replace hello with hi
+let text2 = "hello world, hello everyone";
+console.log(text2.replace("hello","Hi")
+);
+
+
+// remove the middle element from this array
+
+let array = [10,20,30,40,50];
+console.log(array.splice(2,1));
+console.log(array);
